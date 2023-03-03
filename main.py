f01bc6d6ece21a4f6d7c98e8b3f63e520b8dbc8b
md-mahin-islam-mahi <wdmahin.mhm@gmail.com>
2023-03-03 21:48:19 +0600

    Learnt if else condition

--- main.py
+++ main.py
@@ -8,7 +8,19 @@
 # x = input("Enter your name:" )
 # print("Your name is",x)
 
-a = input("Enter first number:")
-b = input("Enter second number:")
-c = float(a) + float(b)
-print("Sum is:", c)
+# a = input("Enter first number:")
+# b = input("Enter second number:")
+# c = float(a) + float(b)
+# print("Sum is:", c)
+
+# is_available = True
+# if is_available == False:
+#      print("He is not available")
+# else:
+#      print("He is available")
+
+goint_to_home = False
+if goint_to_home == True:
+    print("He is going to home.")
+else:
+    print("He is not going to home.")
