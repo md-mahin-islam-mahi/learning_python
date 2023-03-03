d09c986edb559f506fcf51e6b83608bc66eb460b
md-mahin-islam-mahi <wdmahin.mhm@gmail.com>
2023-03-03 21:41:35 +0600

    learnd to convert to int and float

--- main.py
+++ main.py
@@ -5,5 +5,10 @@
 # c = a + " " + b
 # print (c)
 
-x = input("Enter your name:" )
-print("Your name is",x)
+# x = input("Enter your name:" )
+# print("Your name is",x)
+
+a = input("Enter first number:")
+b = input("Enter second number:")
+c = float(a) + float(b)
+print("Sum is:", c)
